9126e5b62514a5a90c4c2c52380e766cacf60b12
Vladimir Fomene <vladimirfomene@gmail.com>
2022-11-12 00:55:43 +0300

    rename disklist.rs to byte_list.rs

--- bdk_core/src/byte_list.rs
+++ bdk_core/src/byte_list.rs
@@ -2,13 +2,13 @@ use alloc::vec::Vec;
 use core::mem::size_of;
 use std::io::{self, Read, Seek, SeekFrom, Write};
 
-/// A very simple collection of `256` on disk linked lists.
+/// A very simple collection of `256` byte linked lists.
 #[derive(Debug)]
-pub struct DiskList<F> {
-    inner: F,
+pub struct ByteList<S> {
+    inner: S,
 }
 
-/// The number of magic mytes
+/// The number of magic bytes
 const MAGIC_BYTES_SIZE: usize = 4;
 
 /// The magic bytes we expect to read at the start of the file
@@ -57,15 +57,16 @@ macro_rules! read_ints {
     }}
 }
 
-impl<F: Read + Write + Seek> DiskList<F> {
-    pub fn load(mut file: F) -> Result<Self, LoadError> {
-        file.rewind()?;
+impl<S: Read + Write + Seek> ByteList<S> {
+    /// Load the byte source ready for Read/Write/Seek
+    pub fn load(mut stream: S) -> Result<Self, LoadError> {
+        stream.rewind()?;
         let mut type_version = [0u8; 4];
-        if let Err(e) = file.read_exact(&mut type_version) {
+        if let Err(e) = stream.read_exact(&mut type_version) {
             if e.kind() == io::ErrorKind::UnexpectedEof {
-                file.rewind()?;
-                file.write_all(&MAGIC_BYTES[..])?;
-                file.write_all(&[0u8; TAIL_TABLE_SIZE])?;
+                stream.rewind()?;
+                stream.write_all(&MAGIC_BYTES)?;
+                stream.write_all(&[0u8; TAIL_TABLE_SIZE])?;
                 type_version = MAGIC_BYTES;
             } else {
                 return Err(LoadError::Io(e));
@@ -76,17 +77,20 @@ impl<F: Read + Write + Seek> DiskList<F> {
             return Err(LoadError::UnexpectedVersion);
         }
 
-        Ok(Self { inner: file })
+        Ok(Self { inner: stream })
     }
 
-    pub fn init(mut file: F) -> Result<Self, io::Error> {
-        file.rewind()?;
-        file.write_all(&MAGIC_BYTES[..])?;
-        file.write_all(&[0u8; TAIL_TABLE_SIZE])?;
+    /// Initialize stream with [`MAGIC_BYTES`] and linked list 
+    /// tail lookup table
+    pub fn init(mut stream: S) -> Result<Self, io::Error> {
+        stream.rewind()?;
+        stream.write_all(&MAGIC_BYTES[..])?;
+        stream.write_all(&[0u8; TAIL_TABLE_SIZE])?;
 
-        Ok(Self { inner: file })
+        Ok(Self { inner: stream })
     }
 
+    /// Use key (object type) to push new serialized changeset to the byte linked list
     pub fn push(&mut self, key: u8, data: &[u8]) -> io::Result<()> {
         assert!(data.len() <= u32::MAX as usize);
         let mut header = [0u8; 12];
@@ -95,19 +99,20 @@ impl<F: Read + Write + Seek> DiskList<F> {
         header[0..8].copy_from_slice(tail_loc.to_be_bytes().as_ref());
         header[8..12].copy_from_slice((data.len() as u32).to_be_bytes().as_ref());
         // Write new entry first so we're sure it's written before we update pointer to tail
-        self.inner.write_all(&header[..])?;
+        self.inner.write_all(&header)?;
         self.inner.write_all(data)?;
         self.inner.flush()?;
         // it worked so let's set the pointer
         self.set_tail_loc(key, new_tail_loc)?;
-        self.inner.flush()?;
         Ok(())
     }
 
+    /// Get the last item in the byte linked list
     pub fn last(&mut self, key: u8) -> io::Result<Option<Vec<u8>>> {
         self.iter(key)?.next().transpose()
     }
 
+    /// Push bytes if last changeset is different from incoming changeset
     pub fn push_if_different(&mut self, key: u8, data: &[u8]) -> io::Result<()> {
         if self.last(key)?.as_ref().map(|x| &x[..]) != Some(data) {
             self.push(key, data)?;
@@ -116,14 +121,15 @@ impl<F: Read + Write + Seek> DiskList<F> {
     }
 
     /// Iterates from the last element to the first
-    pub fn iter(&mut self, key: u8) -> io::Result<IterEntrties<'_, F>> {
+    pub fn iter(&mut self, key: u8) -> io::Result<IterEntries<'_, S>> {
         let last_entry_pos = self.tail_loc(key)?;
-        Ok(IterEntrties {
+        Ok(IterEntries {
             inner: &mut self.inner,
             current: last_entry_pos,
         })
     }
 
+    /// Get all the keys stored in the byte linked list
     pub fn keys(&mut self) -> io::Result<impl DoubleEndedIterator<Item = u8>> {
         self.inner.seek(SeekFrom::Start(MAGIC_BYTES_SIZE as u64))?;
         let mut key_index = [0u8; TAIL_TABLE_SIZE];
@@ -135,6 +141,7 @@ impl<F: Read + Write + Seek> DiskList<F> {
         }))
     }
 
+    /// Move the cursor to the position of the last entry
     fn seek_to_last_entry_pos(&mut self, key: u8) -> io::Result<()> {
         let last_entry_loc =
             SeekFrom::Start(key as u64 * size_of::<u64>() as u64 + MAGIC_BYTES_SIZE as u64);
@@ -142,24 +149,28 @@ impl<F: Read + Write + Seek> DiskList<F> {
         Ok(())
     }
 
+    /// Get the tail location for a particular key (object type)
     fn tail_loc(&mut self, key: u8) -> io::Result<u64> {
         self.seek_to_last_entry_pos(key)?;
         Ok(read_ints!(self.inner => u64))
     }
 
+    /// Set the tail location for a particular key (object type)
     fn set_tail_loc(&mut self, key: u8, pos: u64) -> io::Result<()> {
         self.seek_to_last_entry_pos(key)?;
         self.inner.write_all(pos.to_be_bytes().as_ref())?;
+        self.inner.flush()?;
         Ok(())
     }
 }
 
-pub struct IterEntrties<'a, F> {
-    inner: &'a mut F,
+/// Iterator over entries of the byte linked list
+pub struct IterEntries<'a, S> {
+    inner: &'a mut S,
     current: u64,
 }
 
-impl<'a, F: Read + Write + Seek> Iterator for IterEntrties<'a, F> {
+impl<'a, S: Read + Write + Seek> Iterator for IterEntries<'a, S> {
     type Item = Result<Vec<u8>, io::Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -201,8 +212,8 @@ mod test {
             dbg!(size);
             let mut buf = vec![0u8;size];
             let mut rng = TestRng::deterministic_rng(RngAlgorithm::ChaCha);
-            let mut disklist = match DiskList::init(io::Cursor::new(&mut buf[..])) {
-                Ok(disklist) => disklist,
+            let mut bytelist = match ByteList::init(io::Cursor::new(&mut buf[..])) {
+                Ok(bytelist) => bytelist,
                 Err(_e) => {
                     assert!(size < MAGIC_BYTES_SIZE + TAIL_TABLE_SIZE);
                     return Ok(());
@@ -216,7 +227,7 @@ mod test {
                 let byte_len = rng.gen_range(0..1_000);
                 let mut bytes = vec![0u8;byte_len];
                 rng.fill_bytes(&mut bytes);
-                match disklist.push(key, &bytes) {
+                match bytelist.push(key, &bytes) {
                     Ok(_) => in_memory[key as usize].push_back(bytes),
                     Err(_) => {
                         break
@@ -224,11 +235,11 @@ mod test {
                 }
             }
 
-            let mut disklist = DiskList::load(io::Cursor::new(&mut buf[..])).unwrap();
+            let mut bytelist = ByteList::load(io::Cursor::new(&mut buf[..])).unwrap();
 
             for (key, list) in in_memory.into_iter().enumerate() {
                 let truth = list.into_iter().rev().collect::<Vec<_>>();
-                let our_iter = disklist.iter(key as u8).unwrap().collect::<Result<Vec<_>,_>>().unwrap();
+                let our_iter = bytelist.iter(key as u8).unwrap().collect::<Result<Vec<_>,_>>().unwrap();
                 assert_eq!(truth, our_iter);
             }
         }
@@ -240,34 +251,34 @@ mod test {
         let a2 = b"a2".to_vec();
         let b1 = b"b1".to_vec();
         let target: io::Cursor<Vec<u8>> = io::Cursor::new(vec![]);
-        let mut disk_list = DiskList::load(target).unwrap();
+        let mut byte_list = ByteList::load(target).unwrap();
 
-        assert_eq!(disk_list.iter(0).unwrap().count(), 0);
+        assert_eq!(byte_list.iter(0).unwrap().count(), 0);
 
-        disk_list.push(0, &a1).unwrap();
-        assert_eq!(disk_list.iter(0).unwrap().count(), 1);
-        assert_eq!(disk_list.keys().unwrap().collect::<Vec<_>>(), vec![0]);
+        byte_list.push(0, &a1).unwrap();
+        assert_eq!(byte_list.iter(0).unwrap().count(), 1);
+        assert_eq!(byte_list.keys().unwrap().collect::<Vec<_>>(), vec![0]);
         assert_eq!(
-            disk_list
+            byte_list
                 .iter(0)
                 .unwrap()
                 .collect::<Result<Vec<_>, _>>()
                 .unwrap(),
             vec![a1.clone()]
         );
-        disk_list.push(0, &a2).unwrap();
+        byte_list.push(0, &a2).unwrap();
         assert_eq!(
-            disk_list
+            byte_list
                 .iter(0)
                 .unwrap()
                 .collect::<Result<Vec<_>, _>>()
                 .unwrap(),
             vec![a2.clone(), a1.clone()]
         );
-        disk_list.push(1, &b1).unwrap();
-        assert_eq!(disk_list.keys().unwrap().collect::<Vec<_>>(), vec![0, 1]);
+        byte_list.push(1, &b1).unwrap();
+        assert_eq!(byte_list.keys().unwrap().collect::<Vec<_>>(), vec![0, 1]);
         assert_eq!(
-            disk_list
+            byte_list
                 .iter(0)
                 .unwrap()
                 .collect::<Result<Vec<_>, _>>()
@@ -275,7 +286,7 @@ mod test {
             vec![a2.clone(), a1.clone()]
         );
         assert_eq!(
-            disk_list
+            byte_list
                 .iter(1)
                 .unwrap()
                 .collect::<Result<Vec<_>, _>>()
