32aeef510082cc2e59324f0003bb94f4705c949f
LLFourn <lloyd.fourn@gmail.com>
2022-11-17 14:18:59 +0800

    Remove missing from the concern of chaingraph
    Also don't insert the txid of dangling outputs into the sparse chain. We
    need txids in the sparse chain to have the semantics that they are
    relevent txs that should be fully fetched until we figure out a better
    way to do this.

--- bdk_core/src/chain_graph.rs
+++ bdk_core/src/chain_graph.rs
@@ -2,7 +2,6 @@ use bitcoin::{OutPoint, Transaction, TxOut, Txid};
 use core::fmt::Debug;
 
 use crate::{
-    collections::HashMap,
     sparse_chain::{self, SparseChain},
     tx_graph::{self, TxGraph},
     BlockId, ChainIndex, ConfirmationTime, TxHeight,
@@ -46,15 +45,8 @@ impl<I: ChainIndex> ChainGraph<I> {
         Ok(changed)
     }
 
-    pub fn insert_output(
-        &mut self,
-        outpoint: OutPoint,
-        txout: TxOut,
-        index: I,
-    ) -> Result<bool, sparse_chain::InsertTxErr> {
-        let changed = self.chain.insert_tx(outpoint.txid, index)?;
-        self.graph.insert_txout(outpoint, txout);
-        Ok(changed)
+    pub fn insert_output(&mut self, outpoint: OutPoint, txout: TxOut) -> bool {
+        self.graph.insert_txout(outpoint, txout)
     }
 
     pub fn insert_txid(&mut self, txid: Txid, index: I) -> Result<bool, sparse_chain::InsertTxErr> {
@@ -69,48 +61,31 @@ impl<I: ChainIndex> ChainGraph<I> {
     }
 
     /// Calculates the difference between self and `update` in the form of a [`ChangeSet`].
-    ///
-    /// Additionally, missing transactions are returned in the form of [`HashMap<Txid, bool>`],
-    /// where the value is `true` for partially missing, and `false` for fully missing.
     pub fn determine_changeset(
         &self,
         update: &Self,
-    ) -> Result<(ChangeSet<I>, HashMap<Txid, bool>), sparse_chain::UpdateFailure<I>> {
-        let chain_changeset = self.chain.determine_changeset(&update.chain)?;
-        let graph_additions = self.graph.determine_additions(&update.graph);
-
-        let added_txids = graph_additions.txids().collect::<HashMap<_, _>>();
-
-        let missing = chain_changeset
-            .tx_additions()
-            .filter_map(|txid| match added_txids.get(&txid).cloned() {
-                Some(true) => None,                // not missing
-                Some(false) => Some((txid, true)), // partially missing
-                None => Some((txid, false)),       // completely missing
-            })
-            .collect::<HashMap<_, _>>();
-
-        Ok((
-            ChangeSet::<I> {
-                chain: chain_changeset,
-                graph: graph_additions,
-            },
-            missing,
-        ))
+    ) -> Result<ChangeSet<I>, sparse_chain::UpdateFailure<I>> {
+        Ok(ChangeSet::<I> {
+            chain: self.chain.determine_changeset(&update.chain)?,
+            graph: self.graph.determine_additions(&update.graph),
+        })
     }
 
+    /// Applies a [`ChangeSet`] to the chain graph
     pub fn apply_changeset(&mut self, changeset: &ChangeSet<I>) {
         self.chain.apply_changeset(&changeset.chain);
         self.graph.apply_additions(&changeset.graph);
     }
 
+    /// Applies the `update` chain graph. Note this is shorthand for calling [`determine_changeset`]
+    /// and [`apply_changeset`] in sequence.
     pub fn apply_update(
         &mut self,
         update: &Self,
-    ) -> Result<(ChangeSet<I>, HashMap<Txid, bool>), sparse_chain::UpdateFailure<I>> {
-        let (changeset, missing) = self.determine_changeset(update)?;
+    ) -> Result<ChangeSet<I>, sparse_chain::UpdateFailure<I>> {
+        let changeset = self.determine_changeset(update)?;
         self.apply_changeset(&changeset);
-        Ok((changeset, missing))
+        Ok(changeset)
     }
 }
 
