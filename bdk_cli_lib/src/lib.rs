324e08577f26bd2271fb80bb02c21a52e8148e31
志宇 <hello@evanlinjin.me>
2022-12-23 16:31:55 +0800

    Introduce `bdk_cli::Args::cp_limit`
    This enforces a checkpoint limit on internal `SparseChain`.

--- bdk_cli_lib/src/lib.rs
+++ bdk_cli_lib/src/lib.rs
@@ -36,6 +36,9 @@ pub struct Args<C: clap::Subcommand> {
     #[clap(env = "BDK_DB_DIR", long, default_value = ".bdk_example_db")]
     pub db_dir: PathBuf,
 
+    #[clap(env = "BDK_CP_LIMIT", long, default_value = "20")]
+    pub cp_limit: usize,
+
     #[clap(subcommand)]
     pub command: Commands<C>,
 }
@@ -516,6 +519,8 @@ where
         Descriptor::<DescriptorPublicKey>::parse_descriptor(&secp, &args.descriptor)?;
 
     let mut keychain_tracker = KeychainTracker::default();
+    keychain_tracker.set_checkpoint_limit(Some(args.cp_limit));
+
     keychain_tracker
         .txout_index
         .add_keychain(Keychain::External, descriptor);
@@ -524,7 +529,6 @@ where
         .change_descriptor
         .map(|descriptor| Descriptor::<DescriptorPublicKey>::parse_descriptor(&secp, &descriptor))
         .transpose()?;
-
     if let Some((internal_descriptor, internal_keymap)) = internal {
         keymap.extend(internal_keymap);
         keychain_tracker
