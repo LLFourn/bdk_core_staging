a7ca6a53f67bfe70f4dcd709dbe3e3966912cd0d
志宇 <hello@evanlinjin.me>
2023-02-28 16:52:35 +1300

    Introduce `KeychainPersist`

--- bdk_file_store/src/lib.rs
+++ bdk_file_store/src/lib.rs
@@ -1,2 +1,32 @@
 mod file_store;
+use bdk_chain::{
+    bitcoin::Transaction,
+    keychain::{KeychainChangeSet, KeychainPersistBackend, KeychainTracker},
+    sparse_chain::ChainPosition,
+};
 pub use file_store::*;
+
+impl<'de, K, P> KeychainPersistBackend<K, P> for KeychainStore<K, P>
+where
+    K: Ord + Clone + core::fmt::Debug,
+    P: ChainPosition,
+    KeychainChangeSet<K, P, Transaction>: serde::Serialize + serde::de::DeserializeOwned,
+{
+    type WriteError = std::io::Error;
+
+    type LoadError = IterError;
+
+    fn append_changeset(
+        &mut self,
+        changeset: &KeychainChangeSet<K, P>,
+    ) -> Result<(), Self::WriteError> {
+        KeychainStore::append_changeset(self, changeset)
+    }
+
+    fn load_into_keychain_tracker(
+        &mut self,
+        tracker: &mut KeychainTracker<K, P>,
+    ) -> Result<(), Self::LoadError> {
+        KeychainStore::load_into_keychain_tracker(self, tracker)
+    }
+}
